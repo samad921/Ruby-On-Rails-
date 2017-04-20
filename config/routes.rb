dbaec4137504137d720b8de6f4594de0a9bed702
Abdul Samad <samad921@hotmail.com>
2017-04-20 13:22:12 +0500

    2nd commit

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,10 @@ Rails.application.routes.draw do
    resources :articles
   root 'welcome#index'
 
+  
+resources :articles do
+  resources :comments
+end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
